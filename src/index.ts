4475ef0c88c7a28dce4318546d62fea80a961d00
soohyeok <soohyeok0822@gmail.com>
2021-12-03 16:27:24 -0500

    1.1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash:string;
     public previousHash:string;
     public data:string;
     public timestamp:number;
+
+    static calculateBlockHash = (
+        index:number,
+        previousHash:string,
+        timestamp:number,
+        data:string,
+        ):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(
         index:number,
@@ -20,10 +29,32 @@ class Block {
     }
 }
 
+
 const genesisBlock:Block = new Block(0, "412034012340214", "", "Hello", 123456);
 
-let blockchain:[Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = ():Block[] => blockchain;
+
+const getLatestBlock = ():Block => blockchain[blockchain.length-1];
+
+const getNewTimeStamp = ():number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string): Block => {
+    const previousBlock:Block = getLatestBlock();
+    const newIndex:number = previousBlock.index + 1;
+    const newTimestamp:number = getNewTimeStamp();
+    const newHash:string = Block.calculateBlockHash(
+        newIndex,
+        previousBlock.hash,
+        newTimestamp,
+        data
+        );
+
+    const newBlock:Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("byebye"));
 
 export {}
